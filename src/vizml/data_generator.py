503f96e78e111d3f2d6da54b6db55008a48b58cf
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-10-05 11:53:09 +0530

    added 2D linear data generator and respective tests

--- src/vizml/data_generator.py
+++ src/vizml/data_generator.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from typing import Optional
 
 import numpy as np
 
@@ -28,45 +27,53 @@ class BaseDataGenerator(ABC):
         np.random.seed(seed=self._seed)
 
     @abstractmethod
-    def generate(self, no_of_points: Optional[int] = 1):
+    def generate(self, no_of_points: int = 1):
         """Generates the respective data points."""
+
+
+class NormalDataGenerator(BaseDataGenerator, ABC):
+    """Base class to generate normal data points."""
+
+    @abstractmethod
+    def generate(self, no_of_points: int = 1):
+        """Generates the respective normal data points."""
 
 
 class LinearDataGenerator(BaseDataGenerator, ABC):
     """Base class to generate linear data points."""
 
     @abstractmethod
-    def generate(self, no_of_points: Optional[int] = 10, is_increasing: Optional[bool] = False):
-        """Generates the respective data points."""
+    def generate(self, no_of_points: int = 10, is_increasing: bool = False):
+        """Generates the respective linear data points."""
 
 
-class FloatingPointGenerator(BaseDataGenerator):
+class FloatingPointGenerator(NormalDataGenerator):
 
-    def generate(self, no_of_points: Optional[int] = 1) -> float:
+    def generate(self, no_of_points: int = 1) -> float:
         """Generates a single random float value."""
 
         return np.random.uniform()
 
 
-class Normal1DGenerator(BaseDataGenerator):
+class Normal1DGenerator(NormalDataGenerator):
 
-    def generate(self, no_of_points: Optional[int] = 1):
+    def generate(self, no_of_points: int = 1):
         """Generates an array of random float values."""
 
         return np.random.standard_normal(no_of_points)
 
 
-class Normal2DGenerator(BaseDataGenerator):
+class Normal2DGenerator(NormalDataGenerator):
 
-    def generate(self, no_of_points: Optional[int] = 1):
+    def generate(self, no_of_points: int = 1):
         """Generates a 2D array of random float values."""
 
         return np.random.standard_normal(size=(no_of_points, 2))
 
 
-class Normal3DGenerator(BaseDataGenerator):
+class Normal3DGenerator(NormalDataGenerator):
 
-    def generate(self, no_of_points: Optional[int] = 1):
+    def generate(self, no_of_points: int = 1):
         """Generates a 3D array of random float values."""
 
         return np.random.standard_normal(size=(no_of_points, 3))
@@ -74,7 +81,7 @@ class Normal3DGenerator(BaseDataGenerator):
 
 class Linear1DGenerator(LinearDataGenerator):
 
-    def generate(self, no_of_points: Optional[int] = 10, is_increasing: Optional[bool] = False):
+    def generate(self, no_of_points: int = 10, is_increasing: bool = True):
         """Generates a 1D array of random float values in a linearly ascending or descending order."""
 
         if not is_increasing:
@@ -84,3 +91,20 @@ class Linear1DGenerator(LinearDataGenerator):
         return np.array([i + (np.random.uniform(1.5, 3) * np.random.standard_normal())
                          for i in range(no_of_points)])
 
+
+class Linear2DGenerator(LinearDataGenerator):
+
+    def generate(self, no_of_points: int = 10, is_increasing: bool = True):
+        """Generates a 2D array of random float values in a linearly ascending or descending order."""
+
+        if no_of_points == 0:
+            return np.array([[], []]).transpose()
+
+        if not is_increasing:
+            return np.array([np.array([i + (np.random.uniform(1.5, 3) * np.random.standard_normal()),
+                                       j + (np.random.uniform(1.5, 3) * np.random.standard_normal())])
+                             for i, j in list(zip(range(no_of_points), reversed(range(no_of_points))))])
+
+        return np.array([np.array([i + (np.random.uniform(1.5, 3) * np.random.standard_normal()),
+                                   i + (np.random.uniform(1.5, 3) * np.random.standard_normal())])
+                         for i in range(no_of_points)])
--- tests/test_data_generators/test_linear_2d_generator.py
+++ tests/test_data_generators/test_linear_2d_generator.py
@@ -1,6 +1,6 @@
 import pytest
 from numpy import equal
-from vizml.data_generator import Linear1DGenerator
+from vizml.data_generator import Linear2DGenerator
 
 
 @pytest.mark.parametrize(
@@ -9,8 +9,8 @@ from vizml.data_generator import Linear1DGenerator
 def test_same_intial_generation(no_of_points):
     """New instances must generate same first values as old instances."""
 
-    a = Linear1DGenerator().generate(no_of_points=no_of_points)
-    b = Linear1DGenerator().generate(no_of_points=no_of_points)
+    a = Linear2DGenerator().generate(no_of_points=no_of_points)
+    b = Linear2DGenerator().generate(no_of_points=no_of_points)
 
     assert equal(a, b).all()
 
@@ -18,7 +18,7 @@ def test_same_intial_generation(no_of_points):
 def test_setting_seed():
     """Setting seed should change the seed value."""
 
-    gen = Linear1DGenerator()
+    gen = Linear2DGenerator()
     gen.set_seed(new_seed=7)
 
     assert gen.seed_value == 7
@@ -30,8 +30,8 @@ def test_setting_seed():
 def test_changed_seed_changed_value(no_of_points):
     """Setting a new seed should generate new values."""
 
-    a = Linear1DGenerator().generate(no_of_points=no_of_points)
-    gen = Linear1DGenerator()
+    a = Linear2DGenerator().generate(no_of_points=no_of_points)
+    gen = Linear2DGenerator()
     gen.set_seed(new_seed=127)
     b = gen.generate(no_of_points=no_of_points)
 
@@ -44,19 +44,19 @@ def test_changed_seed_changed_value(no_of_points):
 def test_output_shape(no_of_points):
     """Tests the shape of the data generated."""
 
-    a = Linear1DGenerator().generate(no_of_points=no_of_points)
-    assert a.shape == (no_of_points, )
+    a = Linear2DGenerator().generate(no_of_points=no_of_points)
+    assert a.shape == (no_of_points, 2)
 
 
 def test_increasing():
     """Tests the linearly increasing functionality."""
 
-    a = Linear1DGenerator().generate()
-    assert a[-1] > a[0]
+    a = Linear2DGenerator().generate()
+    assert a[-1][-1], a[0][-1]
 
 
 def test_decreasing():
     """Tests the linearly decreasing functionality."""
 
-    a = Linear1DGenerator().generate(is_increasing=False)
-    assert a[-1] < a[0]
+    a = Linear2DGenerator().generate(is_increasing=False)
+    assert a[0][-1], a[-1][-1]
