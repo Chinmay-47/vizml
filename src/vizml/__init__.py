64a4661aefbeb2b088fd03019568478a5eb73f3f
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-11-01 23:40:31 +0530

    added a global visualize class in init for all visualizations

--- src/vizml/__init__.py
+++ src/vizml/__init__.py
@@ -1,2 +1,16 @@
-from vizml.simple_linear_regression.dashboard import visualize_simple_linear_regression
-from vizml.multi_linear_regression.dashboard import visualize_multi_linear_regression
+from vizml.simple_linear_regression.dashboard import simple_linear_regression_visualizer
+from vizml.multi_linear_regression.dashboard import multi_linear_regression_visualizer
+
+
+class Visualize:
+
+    @staticmethod
+    def simple_linear_regression():
+        """Runs a dashboard on localhost to visualize Simple linear regression."""
+        simple_linear_regression_visualizer.run_server()
+
+    @staticmethod
+    def multi_linear_regression():
+        """Runs a dashboard on localhost to visualize Multi linear regression."""
+        multi_linear_regression_visualizer.run_server()
+
