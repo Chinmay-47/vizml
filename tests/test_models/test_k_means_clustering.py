842d954d956cfcc533a91bb8ec43f7accf7e7dbd
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-11-13 15:36:12 +0530

    added tests for k means clustering

---
+++ tests/test_models/test_k_means_clustering.py
@@ -0,0 +1,26 @@
+from plotly.graph_objects import Figure
+from vizml.k_means_clustering.clustering import KMeansClustering
+
+
+def test_show_data():
+    """Tests the show data function in K means Clustering"""
+
+    clu = KMeansClustering()
+    fig = clu.show_data(return_fig=True)
+    assert isinstance(fig, Figure)
+
+
+def test_show_clusters():
+    """Tests the show clusters function in K means Clustering"""
+
+    clu = KMeansClustering()
+    fig = clu.show_clusters(return_fig=True)
+    assert isinstance(fig, Figure)
+
+
+def test_show_elbow_plot():
+    """Tests the show elbow plot function in K means Clustering"""
+
+    clu = KMeansClustering()
+    fig = clu.show_elbow_plot(return_fig=True)
+    assert isinstance(fig, Figure)
