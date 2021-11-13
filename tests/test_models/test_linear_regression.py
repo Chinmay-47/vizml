55f27d694ff47458c232b16c68cabe5ad0269a80
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-11-13 15:42:19 +0530

    added more tests for simple linear regression

--- tests/test_models/test_linear_regression.py
+++ tests/test_models/test_linear_regression.py
@@ -1,3 +1,4 @@
+from numpy import equal
 from plotly.graph_objects import Figure
 from vizml.simple_linear_regression.regression import (OrdinaryLeastSquaresRegression,
                                                        LassoRegression, RidgeRegression)
@@ -79,3 +80,69 @@ def test_show_error_scores_Ridge():
     reg.train()
     fig = reg.show_regression_line(return_fig=True)
     assert isinstance(fig, Figure)
+
+
+def test_randomize_OLS():
+    """Tests the random initialization of OLS."""
+
+    reg1 = OrdinaryLeastSquaresRegression()
+    reg2 = OrdinaryLeastSquaresRegression(randomize=True)
+    data1 = reg1.data_points
+    data2 = reg2.data_points
+
+    assert not equal(data1, data2).any()
+
+
+def test_randomize_Lasso():
+    """Tests the random initialization of Lasso."""
+
+    reg1 = LassoRegression()
+    reg2 = LassoRegression(randomize=True)
+    data1 = reg1.data_points
+    data2 = reg2.data_points
+
+    assert not equal(data1, data2).any()
+
+
+def test_randomize_Ridge():
+    """Tests the random initialization of Ridge."""
+
+    reg1 = RidgeRegression()
+    reg2 = RidgeRegression(randomize=True)
+    data1 = reg1.data_points
+    data2 = reg2.data_points
+
+    assert not equal(data1, data2).any()
+
+
+def test_random_state_OLS():
+    """Tests the initialization with a fixed random state in OLS."""
+
+    reg1 = OrdinaryLeastSquaresRegression(random_state=7)
+    reg2 = OrdinaryLeastSquaresRegression(random_state=7)
+    data1 = reg1.data_points
+    data2 = reg2.data_points
+
+    assert equal(data1, data2).all()
+
+
+def test_random_state_Lasso():
+    """Tests the initialization with a fixed random state in Lasso."""
+
+    reg1 = LassoRegression(random_state=7)
+    reg2 = LassoRegression(random_state=7)
+    data1 = reg1.data_points
+    data2 = reg2.data_points
+
+    assert equal(data1, data2).all()
+
+
+def test_random_state_Ridge():
+    """Tests the initialization with a fixed random state in Ridge."""
+
+    reg1 = RidgeRegression(random_state=7)
+    reg2 = RidgeRegression(random_state=7)
+    data1 = reg1.data_points
+    data2 = reg2.data_points
+
+    assert equal(data1, data2).all()
