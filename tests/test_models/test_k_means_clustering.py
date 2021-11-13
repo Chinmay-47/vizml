4409e3a361346a954dd27436e1a5ae39f82f3c1a
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-11-13 15:55:18 +0530

    added more tests for k means clustering

--- tests/test_models/test_k_means_clustering.py
+++ tests/test_models/test_k_means_clustering.py
@@ -1,9 +1,10 @@
+from numpy import equal
 from plotly.graph_objects import Figure
 from vizml.k_means_clustering.clustering import KMeansClustering
 
 
 def test_show_data():
-    """Tests the show data function in K means Clustering"""
+    """Tests the show data function in K means Clustering."""
 
     clu = KMeansClustering()
     fig = clu.show_data(return_fig=True)
@@ -11,7 +12,7 @@ def test_show_data():
 
 
 def test_show_clusters():
-    """Tests the show clusters function in K means Clustering"""
+    """Tests the show clusters function in K means Clustering."""
 
     clu = KMeansClustering()
     clu.train()
@@ -20,8 +21,77 @@ def test_show_clusters():
 
 
 def test_show_elbow_plot():
-    """Tests the show elbow plot function in K means Clustering"""
+    """Tests the show elbow plot function in K means Clustering."""
 
     clu = KMeansClustering()
     fig = clu.show_elbow_plot(return_fig=True)
     assert isinstance(fig, Figure)
+
+
+def test_show_data_3d():
+    """Tests the show data function in K means Clustering for 3d config."""
+
+    clu = KMeansClustering(is_3d=True)
+    fig = clu.show_data(return_fig=True)
+    assert isinstance(fig, Figure)
+
+
+def test_show_clusters_3d():
+    """Tests the show clusters function in K means Clustering for 3d config."""
+
+    clu = KMeansClustering(is_3d=True)
+    clu.train()
+    fig = clu.show_clusters(return_fig=True)
+    assert isinstance(fig, Figure)
+
+
+def test_show_elbow_plot_3d():
+    """Tests the show elbow plot function in K means Clustering for 3d config."""
+
+    clu = KMeansClustering(is_3d=True)
+    fig = clu.show_elbow_plot(return_fig=True)
+    assert isinstance(fig, Figure)
+
+
+def test_randomize():
+    """Tests the random initialization of K means Clustering."""
+
+    clu1 = KMeansClustering()
+    clu2 = KMeansClustering(randomize=True)
+    data1 = clu1.data_points
+    data2 = clu2.data_points
+
+    assert not equal(data1, data2).any()
+
+
+def test_random_state():
+    """Tests the initialization with a fixed random state in K means Clustering."""
+
+    clu1 = KMeansClustering(random_state=7)
+    clu2 = KMeansClustering(random_state=7)
+    data1 = clu1.data_points
+    data2 = clu2.data_points
+
+    assert equal(data1, data2).all()
+
+
+def test_randomize_3d():
+    """Tests the random initialization of K means Clustering for 3d config."""
+
+    clu1 = KMeansClustering(is_3d=True)
+    clu2 = KMeansClustering(randomize=True, is_3d=True)
+    data1 = clu1.data_points
+    data2 = clu2.data_points
+
+    assert not equal(data1, data2).any()
+
+
+def test_random_state_3d():
+    """Tests the initialization with a fixed random state in K means Clustering for 3d config."""
+
+    clu1 = KMeansClustering(random_state=7, is_3d=True)
+    clu2 = KMeansClustering(random_state=7, is_3d=True)
+    data1 = clu1.data_points
+    data2 = clu2.data_points
+
+    assert equal(data1, data2).all()
