a7952d0ee412a4e8aa3c48bfef13e6ae07da766f
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-11-27 01:16:50 +0530

    added polynomial regression in Visualize class for easy access

--- src/vizml/__init__.py
+++ src/vizml/__init__.py
@@ -1,6 +1,7 @@
 from vizml.simple_linear_regression.dashboard import simple_linear_regression_visualizer
 from vizml.multi_linear_regression.dashboard import multi_linear_regression_visualizer
 from vizml.k_means_clustering.dashboard import k_means_clustering_visualizer
+from vizml.polynomial_regression.dashboard import polynomial_regression_visualizer
 
 
 class Visualize:
@@ -19,3 +20,8 @@ class Visualize:
     def k_means_clustering():
         """Runs a dashboard on localhost to visualize K Means Clustering."""
         k_means_clustering_visualizer.run_server()
+
+    @staticmethod
+    def polynomial_regression():
+        """Runs a dashboard on localhost to visualize Polynomial Regression."""
+        polynomial_regression_visualizer.run_server()
