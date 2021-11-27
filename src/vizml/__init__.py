f7cc7c2bb24d730001a15f6642794d0b51855391
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-11-28 00:48:52 +0530

    handled name conflicts in init

--- src/vizml/__init__.py
+++ src/vizml/__init__.py
@@ -1,7 +1,7 @@
-from vizml.simple_linear_regression.dashboard import simple_linear_regression_visualizer
-from vizml.multi_linear_regression.dashboard import multi_linear_regression_visualizer
-from vizml.k_means_clustering.dashboard import k_means_clustering_visualizer
-from vizml.polynomial_regression.dashboard import polynomial_regression_visualizer
+from vizml.k_means_clustering.dashboard import DashBoard as DashBoard_KMeansClustering
+from vizml.multi_linear_regression.dashboard import DashBoard as DashBoard_MultiLinearRegression
+from vizml.polynomial_regression.dashboard import DashBoard as DashBoard_PolynomialRegression
+from vizml.simple_linear_regression.dashboard import DashBoard as DashBoard_SimpleLinearRegression
 
 
 class Visualize:
@@ -9,19 +9,19 @@ class Visualize:
     @staticmethod
     def simple_linear_regression():
         """Runs a dashboard on localhost to visualize Simple linear regression."""
-        simple_linear_regression_visualizer.run_server()
+        DashBoard_SimpleLinearRegression().run()
 
     @staticmethod
     def multi_linear_regression():
         """Runs a dashboard on localhost to visualize Multi linear regression."""
-        multi_linear_regression_visualizer.run_server()
+        DashBoard_MultiLinearRegression().run()
 
     @staticmethod
     def k_means_clustering():
         """Runs a dashboard on localhost to visualize K Means Clustering."""
-        k_means_clustering_visualizer.run_server()
+        DashBoard_KMeansClustering().run()
 
     @staticmethod
     def polynomial_regression():
         """Runs a dashboard on localhost to visualize Polynomial Regression."""
-        polynomial_regression_visualizer.run_server()
+        DashBoard_PolynomialRegression().run()
