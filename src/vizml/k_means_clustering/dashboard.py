eea5b7f698973fe84e3eb9993557f455bbf5f780
Chinmaya Srivatsa <csvatsa98@gmail.com>
2021-11-28 00:47:40 +0530

    added all the dashboards into DashBoard classes and made it uniform throughout the project

--- src/vizml/k_means_clustering/dashboard.py
+++ src/vizml/k_means_clustering/dashboard.py
@@ -6,137 +6,146 @@ from vizml._dashboard_configs import DASH_STYLE
 from vizml.k_means_clustering.clustering import KMeansClustering
 
 
-k_means_clustering_visualizer = dash.Dash(name="k_means_clustering")
+class DashBoard:
+    """Class to run a dashboard for K Means Clustering"""
 
-k_means_clustering_visualizer.layout = html.Div([
-    html.H1("K Means Clustering",
-            style=DASH_STYLE),
-    dcc.Tabs(id='plot-tabs', value='tab-1', children=[
-        dcc.Tab(label='Data Points', value='tab-1',
+    _k_means_clustering_visualizer = dash.Dash(name="k_means_clustering")
+
+    _k_means_clustering_visualizer.layout = html.Div([
+        html.H1("K Means Clustering",
                 style=DASH_STYLE),
-        dcc.Tab(label='Clusters', value='tab-2',
-                style=DASH_STYLE),
-        dcc.Tab(label='Elbow Method Plot', value='tab-3',
-                style=DASH_STYLE),
-        dcc.Tab(label='Silhouette Plot', value='tab-4',
-                style=DASH_STYLE),
-        dcc.Tab(label='Average Silhouette Scores', value='tab-5',
-                style=DASH_STYLE),
-    ], style=DASH_STYLE),
-    html.Div([
-        dcc.Slider(
-            id="no-clusters",
-            min=2,
-            max=10,
-            step=1,
-            marks={str(i): "{} clusters".format(i) for i in (3, 6, 9)},
-            tooltip={"placement": "bottom", "always_visible": False},
-            value=3,
-            dots=False)
-    ], style={**DASH_STYLE, **{'margin-top': '5px', 'margin-bottom': '5px'}}),
-    html.Div([
-        dcc.Slider(
-            id="no-points",
-            min=50,
-            max=300,
-            step=1,
-            marks={str(i): "{} points".format(i) for i in range(50, 310, 50)},
-            tooltip={"placement": "bottom", "always_visible": False},
-            value=100,
-            dots=False)
-    ], style={**DASH_STYLE, **{'margin-top': '5px', 'margin-bottom': '5px'}}),
-    html.Div([
-        dcc.RadioItems(
-            id='randomize',
-            options=[
-                {'label': 'Initial', 'value': 'initial'},
-                {'label': 'Random', 'value': 'random'},
-            ],
-            value='initial',
-            style={'display': 'inline-block', 'width': '40%'}
-        ),
-        dcc.RadioItems(
-            id='no-dimensions',
-            options=[
-                {'label': '2D', 'value': '2d'},
-                {'label': '3D', 'value': '3d'},
-            ],
-            value='2d',
-            style={'display': 'inline-block', 'width': '40%'}
-        )
-    ], style=DASH_STYLE),
-    dcc.Graph('plot'),
-    dcc.Store(id='random-state'),
-    dcc.Store(id='plot1'),
-    dcc.Store(id='plot2'),
-    dcc.Store(id='plot3'),
-    dcc.Store(id='plot4'),
-    dcc.Store(id='plot5')
-], style=DASH_STYLE)
+        dcc.Tabs(id='plot-tabs', value='tab-1', children=[
+            dcc.Tab(label='Data Points', value='tab-1',
+                    style=DASH_STYLE),
+            dcc.Tab(label='Clusters', value='tab-2',
+                    style=DASH_STYLE),
+            dcc.Tab(label='Elbow Method Plot', value='tab-3',
+                    style=DASH_STYLE),
+            dcc.Tab(label='Silhouette Plot', value='tab-4',
+                    style=DASH_STYLE),
+            dcc.Tab(label='Average Silhouette Scores', value='tab-5',
+                    style=DASH_STYLE),
+        ], style=DASH_STYLE),
+        html.Div([
+            dcc.Slider(
+                id="no-clusters",
+                min=2,
+                max=10,
+                step=1,
+                marks={str(i): "{} clusters".format(i) for i in (3, 6, 9)},
+                tooltip={"placement": "bottom", "always_visible": False},
+                value=3,
+                dots=False)
+        ], style={**DASH_STYLE, **{'margin-top': '5px', 'margin-bottom': '5px'}}),
+        html.Div([
+            dcc.Slider(
+                id="no-points",
+                min=50,
+                max=300,
+                step=1,
+                marks={str(i): "{} points".format(i) for i in range(50, 310, 50)},
+                tooltip={"placement": "bottom", "always_visible": False},
+                value=100,
+                dots=False)
+        ], style={**DASH_STYLE, **{'margin-top': '5px', 'margin-bottom': '5px'}}),
+        html.Div([
+            dcc.RadioItems(
+                id='randomize',
+                options=[
+                    {'label': 'Initial', 'value': 'initial'},
+                    {'label': 'Random', 'value': 'random'},
+                ],
+                value='initial',
+                style={'display': 'inline-block', 'width': '40%'}
+            ),
+            dcc.RadioItems(
+                id='no-dimensions',
+                options=[
+                    {'label': '2D', 'value': '2d'},
+                    {'label': '3D', 'value': '3d'},
+                ],
+                value='2d',
+                style={'display': 'inline-block', 'width': '40%'}
+            )
+        ], style=DASH_STYLE),
+        dcc.Graph('plot'),
+        dcc.Store(id='random-state'),
+        dcc.Store(id='plot1'),
+        dcc.Store(id='plot2'),
+        dcc.Store(id='plot3'),
+        dcc.Store(id='plot4'),
+        dcc.Store(id='plot5')
+    ], style=DASH_STYLE)
 
+    @staticmethod
+    @_k_means_clustering_visualizer.callback(
+        Output(component_id='random-state', component_property='value'),
+        Input(component_id='randomize', component_property='value')
+    )
+    def _update_random_state(random_val):
+        """Sets a random state based on the input."""
 
-@k_means_clustering_visualizer.callback(
-    Output(component_id='random-state', component_property='value'),
-    Input(component_id='randomize', component_property='value')
-)
-def update_random_state(random_val):
-    """Sets a random state based on the input."""
+        if random_val == 'random':
+            np.random.seed(seed=None)
+            random_state = np.random.randint(low=1, high=100000)
+            return random_state
 
-    if random_val == 'random':
-        np.random.seed(seed=None)
-        random_state = np.random.randint(low=1, high=100000)
-        return random_state
+        return -1
 
-    return -1
+    @staticmethod
+    @_k_means_clustering_visualizer.callback(
+        Output(component_id='plot1', component_property='figure'),
+        Output(component_id='plot2', component_property='figure'),
+        Output(component_id='plot3', component_property='figure'),
+        Output(component_id='plot4', component_property='figure'),
+        Output(component_id='plot5', component_property='figure'),
+        Input(component_id='random-state', component_property='value'),
+        Input(component_id='no-points', component_property='value'),
+        Input(component_id='no-clusters', component_property='value'),
+        Input(component_id='no-dimensions', component_property='value')
+    )
+    def _init_clustering(random_state, num_points, num_clusters, num_dim):
+        """Initialize K Means Clustering and store all the plots."""
 
+        is_3d = False if num_dim == '2d' else True
 
-@k_means_clustering_visualizer.callback(
-    Output(component_id='plot1', component_property='figure'),
-    Output(component_id='plot2', component_property='figure'),
-    Output(component_id='plot3', component_property='figure'),
-    Output(component_id='plot4', component_property='figure'),
-    Output(component_id='plot5', component_property='figure'),
-    Input(component_id='random-state', component_property='value'),
-    Input(component_id='no-points', component_property='value'),
-    Input(component_id='no-clusters', component_property='value'),
-    Input(component_id='no-dimensions', component_property='value')
-)
-def init_clustering(random_state, num_points, num_clusters, num_dim):
-    """"Initialize K Means Clustering and store all the plots."""
+        clu = KMeansClustering(no_points=num_points, no_clusters=num_clusters, random_state=random_state,
+                               is_3d=is_3d)
+        clu.train()
 
-    is_3d = False if num_dim == '2d' else True
+        return (clu.show_data(return_fig=True), clu.show_clusters(return_fig=True),
+                clu.show_elbow_plot(return_fig=True), clu.show_silhouette_plot(return_fig=True),
+                clu.show_avg_silhouette_scores(return_fig=True))
 
-    clu = KMeansClustering(no_points=num_points, no_clusters=num_clusters, random_state=random_state,
-                           is_3d=is_3d)
-    clu.train()
+    @staticmethod
+    @_k_means_clustering_visualizer.callback(
+        Output(component_id='plot', component_property='figure'),
+        Input(component_id='plot-tabs', component_property='value'),
+        Input(component_id='plot1', component_property='figure'),
+        Input(component_id='plot2', component_property='figure'),
+        Input(component_id='plot3', component_property='figure'),
+        Input(component_id='plot4', component_property='figure'),
+        Input(component_id='plot5', component_property='figure')
+    )
+    def _update_plots(plot_tab, fig1, fig2, fig3, fig4, fig5):
+        """Updates the plot based on the chosen tab."""
 
-    return (clu.show_data(return_fig=True), clu.show_clusters(return_fig=True), clu.show_elbow_plot(return_fig=True),
-            clu.show_silhouette_plot(return_fig=True), clu.show_avg_silhouette_scores(return_fig=True))
+        if plot_tab == "tab-1":
+            return fig1
+        elif plot_tab == "tab-2":
+            return fig2
+        elif plot_tab == "tab-3":
+            return fig3
+        elif plot_tab == "tab-4":
+            return fig4
+        else:
+            return fig5
 
+    def run(self):
+        """Runs a dashboard on localhost to visualize K Means Clustering."""
 
-@k_means_clustering_visualizer.callback(
-    Output(component_id='plot', component_property='figure'),
-    Input(component_id='plot-tabs', component_property='value'),
-    Input(component_id='plot1', component_property='figure'),
-    Input(component_id='plot2', component_property='figure'),
-    Input(component_id='plot3', component_property='figure'),
-    Input(component_id='plot4', component_property='figure'),
-    Input(component_id='plot5', component_property='figure')
-)
-def update_plots(plot_tab, fig1, fig2, fig3, fig4, fig5):
-    """Updates the plot based on the chosen tab."""
-
-    if plot_tab == "tab-1":
-        return fig1
-    elif plot_tab == "tab-2":
-        return fig2
-    elif plot_tab == "tab-3":
-        return fig3
-    elif plot_tab == "tab-4":
-        return fig4
-    else:
-        return fig5
+        self._k_means_clustering_visualizer.run_server()
 
 
 if __name__ == '__main__':
-    k_means_clustering_visualizer.run_server()
+    DashBoard().run()
--- src/vizml/simple_linear_regression/dashboard.py
+++ src/vizml/simple_linear_regression/dashboard.py
@@ -9,144 +9,159 @@ from vizml.simple_linear_regression.regression import (OrdinaryLeastSquaresRegre
                                                        RidgeRegression)
 
 
-simple_linear_regression_visualizer = dash.Dash(name="simple_linear_regression")
+class DashBoard:
+    """Class to run a dashboard for Simple Linear Regression."""
 
-simple_linear_regression_visualizer.layout = html.Div([
-    html.H1("Simple Linear Regression",
-            style=DASH_STYLE),
-    html.Div([
-        dcc.Tabs(id='type-tabs', value='tab-1', children=[
-            dcc.Tab(label='Ordinary Least Squares Regression', value='tab-1',
-                    style=DASH_STYLE),
-            dcc.Tab(label='Lasso Regression', value='tab-2',
-                    style=DASH_STYLE),
-            dcc.Tab(label='Ridge Regression', value='tab-3',
-                    style=DASH_STYLE),
-        ], style=DASH_STYLE)
-    ]),
-    dcc.Tabs(id='plot-tabs', value='tab-1', children=[
-        dcc.Tab(label='Data Points', value='tab-1',
+    _simple_linear_regression_visualizer = dash.Dash(name="simple_linear_regression")
+
+    _simple_linear_regression_visualizer.layout = html.Div([
+        html.H1("Simple Linear Regression",
                 style=DASH_STYLE),
-        dcc.Tab(label='Regression Line', value='tab-2',
-                style=DASH_STYLE),
-        dcc.Tab(label='Error Metrics', value='tab-3',
-                style=DASH_STYLE),
-    ], style=DASH_STYLE),
-    html.Div([
-        dcc.Slider(
-            id="no-points",
-            min=10,
-            max=100,
-            step=1,
-            marks={str(i): str(i) for i in range(10, 110, 10)},
-            tooltip={"placement": "bottom", "always_visible": False},
-            value=10,
-            dots=False)
-    ], style={**DASH_STYLE, **{'margin-top': '5px', 'margin-bottom': '5px'}}),
-    html.Div([
-        dcc.RadioItems(
-            id='randomize',
-            options=[
-                {'label': 'Initial', 'value': 'initial'},
-                {'label': 'Random', 'value': 'random'},
-            ],
-            value='initial',
-            style={'display': 'inline-block', 'width': '40%'}
-        ),
-        dcc.RadioItems(
-            id='linearly-increasing',
-            options=[
-                {'label': 'Increasing', 'value': 'increasing'},
-                {'label': 'Decreasing', 'value': 'decreasing'},
-            ],
-            value='increasing',
-            style={'display': 'inline-block', 'width': '40%'}
-        )
-    ], style=DASH_STYLE),
-    dcc.Graph('plot'),
-    dcc.Store(id='plot1'),
-    dcc.Store(id='plot2'),
-    dcc.Store(id='plot3'),
-    dcc.Store(id='plot4'),
-    dcc.Store(id='plot5'),
-    dcc.Store(id='plot6'),
-    dcc.Store(id='plot7'),
-    dcc.Store(id='plot8'),
-    dcc.Store(id='plot9')
-], style=DASH_STYLE)
+        html.Div([
+            dcc.Tabs(id='type-tabs', value='tab-1', children=[
+                dcc.Tab(label='Ordinary Least Squares Regression', value='tab-1',
+                        style=DASH_STYLE),
+                dcc.Tab(label='Lasso Regression', value='tab-2',
+                        style=DASH_STYLE),
+                dcc.Tab(label='Ridge Regression', value='tab-3',
+                        style=DASH_STYLE),
+            ], style=DASH_STYLE)
+        ]),
+        dcc.Tabs(id='plot-tabs', value='tab-1', children=[
+            dcc.Tab(label='Data Points', value='tab-1',
+                    style=DASH_STYLE),
+            dcc.Tab(label='Regression Line', value='tab-2',
+                    style=DASH_STYLE),
+            dcc.Tab(label='Error Metrics', value='tab-3',
+                    style=DASH_STYLE),
+        ], style=DASH_STYLE),
+        html.Div([
+            dcc.Slider(
+                id="no-points",
+                min=10,
+                max=100,
+                step=1,
+                marks={str(i): str(i) for i in range(10, 110, 10)},
+                tooltip={"placement": "bottom", "always_visible": False},
+                value=10,
+                dots=False)
+        ], style={**DASH_STYLE, **{'margin-top': '5px', 'margin-bottom': '5px'}}),
+        html.Div([
+            dcc.RadioItems(
+                id='randomize',
+                options=[
+                    {'label': 'Initial', 'value': 'initial'},
+                    {'label': 'Random', 'value': 'random'},
+                ],
+                value='initial',
+                style={'display': 'inline-block', 'width': '40%'}
+            ),
+            dcc.RadioItems(
+                id='linearly-increasing',
+                options=[
+                    {'label': 'Increasing', 'value': 'increasing'},
+                    {'label': 'Decreasing', 'value': 'decreasing'},
+                ],
+                value='increasing',
+                style={'display': 'inline-block', 'width': '40%'}
+            )
+        ], style=DASH_STYLE),
+        dcc.Graph('plot'),
+        dcc.Store(id='random-state'),
+        dcc.Store(id='plot1'),
+        dcc.Store(id='plot2'),
+        dcc.Store(id='plot3'),
+        dcc.Store(id='plot4'),
+        dcc.Store(id='plot5'),
+        dcc.Store(id='plot6'),
+        dcc.Store(id='plot7'),
+        dcc.Store(id='plot8'),
+        dcc.Store(id='plot9')
+    ], style=DASH_STYLE)
 
+    @staticmethod
+    @_simple_linear_regression_visualizer.callback(
+        Output(component_id='random-state', component_property='value'),
+        Input(component_id='randomize', component_property='value')
+    )
+    def _update_random_state(random_val):
+        """Sets a random state based on the input."""
 
-@simple_linear_regression_visualizer.callback(
-    Output(component_id='plot1', component_property='figure'),
-    Output(component_id='plot2', component_property='figure'),
-    Output(component_id='plot3', component_property='figure'),
-    Output(component_id='plot4', component_property='figure'),
-    Output(component_id='plot5', component_property='figure'),
-    Output(component_id='plot6', component_property='figure'),
-    Output(component_id='plot7', component_property='figure'),
-    Output(component_id='plot8', component_property='figure'),
-    Output(component_id='plot9', component_property='figure'),
-    Input(component_id='randomize', component_property='value'),
-    Input(component_id='no-points', component_property='value'),
-    Input(component_id='linearly-increasing', component_property='value')
-)
-def init_regressors(is_random, no_points, is_inc):
-    """Initializes the regressor and stores the initial plots."""
+        if random_val == 'random':
+            np.random.seed(seed=None)
+            random_state = np.random.randint(low=1, high=100000)
+            return random_state
 
-    randomize = True if is_random == 'random' else False
-    is_increasing = True if is_inc == 'increasing' else False
+        return -1
 
-    if not randomize:
-        reg1 = LassoRegression(no_points=no_points, is_increasing=is_increasing)
-        reg2 = RidgeRegression(no_points=no_points, is_increasing=is_increasing)
-        reg3 = OrdinaryLeastSquaresRegression(no_points=no_points, is_increasing=is_increasing)
+    @staticmethod
+    @_simple_linear_regression_visualizer.callback(
+        Output(component_id='plot1', component_property='figure'),
+        Output(component_id='plot2', component_property='figure'),
+        Output(component_id='plot3', component_property='figure'),
+        Output(component_id='plot4', component_property='figure'),
+        Output(component_id='plot5', component_property='figure'),
+        Output(component_id='plot6', component_property='figure'),
+        Output(component_id='plot7', component_property='figure'),
+        Output(component_id='plot8', component_property='figure'),
+        Output(component_id='plot9', component_property='figure'),
+        Input(component_id='random-state', component_property='value'),
+        Input(component_id='no-points', component_property='value'),
+        Input(component_id='linearly-increasing', component_property='value')
+    )
+    def _init_regressors(random_state, no_points, is_inc):
+        """Initializes the regressor and stores the initial plots."""
 
-    else:
-        np.random.seed(seed=None)
-        random_state = np.random.randint(low=1, high=100000)
+        is_increasing = True if is_inc == 'increasing' else False
+
         reg1 = LassoRegression(no_points=no_points, is_increasing=is_increasing, random_state=random_state)
         reg2 = RidgeRegression(no_points=no_points, is_increasing=is_increasing, random_state=random_state)
         reg3 = OrdinaryLeastSquaresRegression(no_points=no_points, is_increasing=is_increasing,
                                               random_state=random_state)
 
-    reg1.train()
-    reg2.train()
-    reg3.train()
+        reg1.train()
+        reg2.train()
+        reg3.train()
 
-    return (reg1.show_data(return_fig=True), reg1.show_regression_line(return_fig=True),
-            reg1.show_error_scores(return_fig=True), reg2.show_data(return_fig=True),
-            reg2.show_regression_line(return_fig=True), reg2.show_error_scores(return_fig=True),
-            reg3.show_data(return_fig=True), reg3.show_regression_line(return_fig=True),
-            reg3.show_error_scores(return_fig=True))
+        return (reg1.show_data(return_fig=True), reg1.show_regression_line(return_fig=True),
+                reg1.show_error_scores(return_fig=True), reg2.show_data(return_fig=True),
+                reg2.show_regression_line(return_fig=True), reg2.show_error_scores(return_fig=True),
+                reg3.show_data(return_fig=True), reg3.show_regression_line(return_fig=True),
+                reg3.show_error_scores(return_fig=True))
 
+    @staticmethod
+    @_simple_linear_regression_visualizer.callback(
+        Output(component_id='plot', component_property='figure'),
+        Input(component_id='plot-tabs', component_property='value'),
+        Input(component_id='type-tabs', component_property='value'),
+        Input(component_id='plot1', component_property='figure'),
+        Input(component_id='plot2', component_property='figure'),
+        Input(component_id='plot3', component_property='figure'),
+        Input(component_id='plot4', component_property='figure'),
+        Input(component_id='plot5', component_property='figure'),
+        Input(component_id='plot6', component_property='figure'),
+        Input(component_id='plot7', component_property='figure'),
+        Input(component_id='plot8', component_property='figure'),
+        Input(component_id='plot9', component_property='figure')
+    )
+    def _update_graph(plot_tab, type_tab, plot1, plot2, plot3, plot4, plot5, plot6, plot7, plot8, plot9):
+        """Switches plot based on selection."""
 
-@simple_linear_regression_visualizer.callback(
-    Output(component_id='plot', component_property='figure'),
-    Input(component_id='plot-tabs', component_property='value'),
-    Input(component_id='type-tabs', component_property='value'),
-    Input(component_id='plot1', component_property='figure'),
-    Input(component_id='plot2', component_property='figure'),
-    Input(component_id='plot3', component_property='figure'),
-    Input(component_id='plot4', component_property='figure'),
-    Input(component_id='plot5', component_property='figure'),
-    Input(component_id='plot6', component_property='figure'),
-    Input(component_id='plot7', component_property='figure'),
-    Input(component_id='plot8', component_property='figure'),
-    Input(component_id='plot9', component_property='figure')
-)
-def update_graph(plot_tab, type_tab, plot1, plot2, plot3, plot4, plot5, plot6, plot7, plot8, plot9):
-    """Switches plot based on selection"""
+        all_plots = [plot1, plot2, plot3, plot4, plot5, plot6, plot7, plot8, plot9]
 
-    all_plots = [plot1, plot2, plot3, plot4, plot5, plot6, plot7, plot8, plot9]
+        tab_types = ['tab-1', 'tab-2', 'tab-3']
+        combs = list(itertools.product(*[tab_types, tab_types]))
+        for i, comb in enumerate(combs):
+            if (type_tab, plot_tab) == comb:
+                return all_plots[i]
 
-    tab_types = ['tab-1', 'tab-2', 'tab-3']
-    combs = list(itertools.product(*[tab_types, tab_types]))
-    for i, comb in enumerate(combs):
-        if (type_tab, plot_tab) == comb:
-            return all_plots[i]
+        return plot1
 
-    return plot1
+    def run(self):
+        """Runs a dashboard on localhost to visualize Simple Linear Regression."""
+
+        self._simple_linear_regression_visualizer.run_server()
 
 
 if __name__ == '__main__':
-    simple_linear_regression_visualizer.run_server()
+    DashBoard().run()
