23b1f76d5b3313d5aad8373367e44b7d94b0f863
Chinmaya Srivatsa <csvatsa98@gmail.com>
2022-01-10 18:00:36 +0530

    added LinearlySeparable2DGenerator and LinearlySeparable3DGenerator for generating data for classification

--- src/vizml/data_generator.py
+++ src/vizml/data_generator.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 import numpy as np
+from sklearn.datasets import make_classification
 
 
 class BaseDataGenerator(ABC):
@@ -50,6 +51,14 @@ class LinearDataGenerator(BaseDataGenerator):
     @abstractmethod
     def generate(self, no_of_points: int = 10, is_increasing: bool = False):
         """Generates the respective linear data points."""
+
+
+class ClassificationDataGenerator(BaseDataGenerator):
+    """Base class to generate data for classification."""
+
+    @abstractmethod
+    def generate(self, no_of_points: int = 10, no_classes: int = 2):
+        """Generates the respective data points for classification."""
 
 
 class FloatingPointGenerator(NormalDataGenerator):
@@ -136,3 +145,31 @@ class Linear3DGenerator(LinearDataGenerator):
                                    i + (np.random.uniform(1.75, 7) * np.random.standard_normal()),
                                    i + (np.random.uniform(1.75, 7) * np.random.standard_normal())])
                          for i in range(no_of_points)])
+
+
+class LinearlySeparable2DGenerator(ClassificationDataGenerator):
+
+    def generate(self, no_of_points: int = 10, no_classes: int = 2):
+        """Generates linearly separable 2D data along with labels for classification."""
+
+        if no_of_points == 0:
+            return np.array([[], [], []]).transpose()
+
+        x, y = make_classification(n_samples=no_of_points, n_features=2, n_redundant=0, n_informative=2,
+                                   random_state=self.seed_value, n_classes=no_classes, n_clusters_per_class=1)
+
+        return np.array(list(zip(*(x[:, 0], x[:, 1], y))))
+
+
+class LinearlySeparable3DGenerator(ClassificationDataGenerator):
+
+    def generate(self, no_of_points: int = 10, no_classes: int = 2):
+        """Generates linearly separable 3D data along with labels for classification."""
+
+        if no_of_points == 0:
+            return np.array([[], [], [], []]).transpose()
+
+        x, y = make_classification(n_samples=no_of_points, n_features=3, n_redundant=0, n_informative=3,
+                                   random_state=self.seed_value, n_classes=no_classes, n_clusters_per_class=1)
+
+        return np.array(list(zip(*(x[:, 0], x[:, 1], x[:, 2], y))))
