bb62835a1e7e816babb0f4c2eee7f0b61d47633b
Chinmaya Srivatsa <csvatsa98@gmail.com>
2022-01-25 00:50:29 +0530

    updated tests for knn classifier

--- tests/test_models/test_k_nearest_neighbors.py
+++ tests/test_models/test_k_nearest_neighbors.py
@@ -14,7 +14,7 @@ def test_show_data():
 def test_show_decision_boundary():
     """Tests the show decision boundary function in K Nearest Neighbours classifier."""
 
-    clf = KNearestNeighbours(no_points=10)
+    clf = KNearestNeighbours(no_points=10, k_neighbors=2)
     clf.train()
     fig = clf.show_decision_boundary(return_fig=True)
     assert isinstance(fig, Figure)
@@ -58,7 +58,7 @@ def test_show_data_3d():
 def test_show_decision_boundary_3d():
     """Tests the show decision boundary function in K Nearest Neighbours classifier for 3d config."""
 
-    clf = KNearestNeighbours(is_3d=True, no_points=10)
+    clf = KNearestNeighbours(is_3d=True, no_points=10, k_neighbors=2)
     clf.train()
     fig = clf.show_decision_boundary(return_fig=True)
     assert isinstance(fig, Figure)
