7a055fc4692a4ba30248022e7321b598de34221d
Chinmaya Srivatsa <csvatsa98@gmail.com>
2022-01-25 17:06:08 +0530

    added tests for naive bayes

---
+++ tests/test_models/test_naive_bayes.py
@@ -0,0 +1,241 @@
+from numpy import equal, ndarray
+from plotly.graph_objects import Figure
+from vizml.naive_bayes.classification import NaiveBayes
+
+
+def test_show_data():
+    """Tests the show data function in Naive Bayes."""
+
+    clf = NaiveBayes(no_points=10)
+    fig = clf.show_data(return_fig=True)
+    assert isinstance(fig, Figure)
+
+
+def test_show_decision_boundary():
+    """Tests the show decision boundary function in Naive Bayes."""
+
+    clf = NaiveBayes(no_points=10)
+    clf.train()
+    fig = clf.show_decision_boundary(return_fig=True)
+    assert isinstance(fig, Figure)
+
+
+def test_show_decision_probabilities():
+    """Tests the show decision probabilities function in Naive Bayes."""
+
+    clf = NaiveBayes(no_points=10)
+    clf.train()
+    fig = clf.show_decision_probabilities(return_fig=True)
+    assert isinstance(fig, Figure)
+
+
+def test_show_metrics():
+    """Tests the show metrics function in Naive Bayes."""
+
+    clf = NaiveBayes(no_points=10)
+    clf.train()
+    fig = clf.show_metrics(return_fig=True)
+    assert isinstance(fig, Figure)
+
+
+def test_show_confusion_matrix():
+    """Tests the show confusion matrix function in Naive Bayes."""
+
+    clf = NaiveBayes(no_points=10)
+    clf.train()
+    fig = clf.show_confusion_matrix(return_fig=True)
+    assert isinstance(fig, Figure)
+
+
+def test_show_data_3d():
+    """Tests the show data function in Naive Bayes for 3d config."""
+
+    clf = NaiveBayes(is_3d=True, no_points=10)
+    fig = clf.show_data(return_fig=True)
+    assert isinstance(fig, Figure)
+
+
+def test_show_decision_boundary_3d():
+    """Tests the show decision boundary function in Naive Bayes for 3d config."""
+
+    clf = NaiveBayes(is_3d=True, no_points=10)
+    clf.train()
+    fig = clf.show_decision_boundary(return_fig=True)
+    assert isinstance(fig, Figure)
+
+
+def test_show_decision_probabilities_3d():
+    """Tests the show decision probabilities function in Naive Bayes for 3d config."""
+
+    clf = NaiveBayes(is_3d=True, no_points=10)
+    clf.train()
+    fig = clf.show_decision_probabilities(return_fig=True)
+    assert isinstance(fig, Figure)
+
+
+def test_show_metrics_3d():
+    """Tests the show metrics function in Naive Bayes for 3d config."""
+
+    clf = NaiveBayes(is_3d=True, no_points=10)
+    clf.train()
+    fig = clf.show_metrics(return_fig=True)
+    assert isinstance(fig, Figure)
+
+
+def test_show_confusion_matrix_3d():
+    """Tests the show silhouette plot function in Naive Bayes for 3d config."""
+
+    clf = NaiveBayes(is_3d=True, no_points=10)
+    clf.train()
+    fig = clf.show_confusion_matrix(return_fig=True)
+    assert isinstance(fig, Figure)
+
+
+def test_randomize():
+    """Tests the random initialization of Naive Bayes."""
+
+    clf1 = NaiveBayes()
+    clf2 = NaiveBayes(randomize=True)
+    data1 = clf1.data_points
+    data2 = clf2.data_points
+
+    assert not equal(data1, data2).any()
+
+
+def test_random_state():
+    """Tests the initialization with a fixed random state in Naive Bayes."""
+
+    clf1 = NaiveBayes(random_state=7)
+    clf2 = NaiveBayes(random_state=7)
+    data1 = clf1.data_points
+    data2 = clf2.data_points
+
+    assert equal(data1, data2).all()
+
+
+def test_randomize_3d():
+    """Tests the random initialization of Naive Bayes for 3d config."""
+
+    clf1 = NaiveBayes(is_3d=True)
+    clf2 = NaiveBayes(randomize=True, is_3d=True)
+    data1 = clf1.data_points
+    data2 = clf2.data_points
+
+    assert not equal(data1, data2).any()
+
+
+def test_random_state_3d():
+    """Tests the initialization with a fixed random state in Naive Bayes for 3d config."""
+
+    clf1 = NaiveBayes(random_state=7, is_3d=True)
+    clf2 = NaiveBayes(random_state=7, is_3d=True)
+    data1 = clf1.data_points
+    data2 = clf2.data_points
+
+    assert equal(data1, data2).all()
+
+
+def test_randomize_moon():
+    """Tests the random initialization of Naive Bayes with moon data."""
+
+    clf1 = NaiveBayes(data_shape='moon')
+    clf2 = NaiveBayes(randomize=True, data_shape='moon')
+    data1 = clf1.data_points
+    data2 = clf2.data_points
+
+    assert not equal(data1, data2).any()
+
+
+def test_random_state_moon():
+    """Tests the initialization with a fixed random state in Naive Bayes with moon data."""
+
+    clf1 = NaiveBayes(random_state=7, data_shape='moon')
+    clf2 = NaiveBayes(random_state=7, data_shape='moon')
+    data1 = clf1.data_points
+    data2 = clf2.data_points
+
+    assert equal(data1, data2).all()
+
+
+def test_randomize_3d_moon():
+    """Tests the random initialization of Naive Bayes for 3d config with moon data."""
+
+    clf1 = NaiveBayes(is_3d=True, data_shape='moon')
+    clf2 = NaiveBayes(randomize=True, is_3d=True, data_shape='moon')
+    data1 = clf1.data_points
+    data2 = clf2.data_points
+
+    assert not equal(data1, data2).any()
+
+
+def test_random_state_3d_moon():
+    """Tests the initialization with a fixed random state in Naive Bayes for 3d config with moon data."""
+
+    clf1 = NaiveBayes(random_state=7, is_3d=True, data_shape='moon')
+    clf2 = NaiveBayes(random_state=7, is_3d=True, data_shape='moon')
+    data1 = clf1.data_points
+    data2 = clf2.data_points
+
+    assert equal(data1, data2).all()
+
+
+def test_randomize_circle():
+    """Tests the random initialization of Naive Bayes with circle data."""
+
+    clf1 = NaiveBayes(data_shape='moon')
+    clf2 = NaiveBayes(randomize=True, data_shape='moon')
+    data1 = clf1.data_points
+    data2 = clf2.data_points
+
+    assert not equal(data1, data2).any()
+
+
+def test_random_state_circle():
+    """Tests the initialization with a fixed random state in Naive Bayes with circle data."""
+
+    clf1 = NaiveBayes(random_state=7, data_shape='circle')
+    clf2 = NaiveBayes(random_state=7, data_shape='circle')
+    data1 = clf1.data_points
+    data2 = clf2.data_points
+
+    assert equal(data1, data2).all()
+
+
+def test_randomize_3d_circle():
+    """Tests the random initialization of Naive Bayes for 3d config with circle data."""
+
+    clf1 = NaiveBayes(is_3d=True, data_shape='circle')
+    clf2 = NaiveBayes(randomize=True, is_3d=True, data_shape='circle')
+    data1 = clf1.data_points
+    data2 = clf2.data_points
+
+    assert not equal(data1, data2).any()
+
+
+def test_random_state_3d_circle():
+    """Tests the initialization with a fixed random state in Naive Bayes for 3d config with circle data."""
+
+    clf1 = NaiveBayes(random_state=7, is_3d=True, data_shape='circle')
+    clf2 = NaiveBayes(random_state=7, is_3d=True, data_shape='circle')
+    data1 = clf1.data_points
+    data2 = clf2.data_points
+
+    assert equal(data1, data2).all()
+
+
+def test_decision_function():
+    """Tests the decision function property for Naive Bayes."""
+
+    clf = NaiveBayes()
+    clf.train()
+
+    assert isinstance(clf.decision_function, ndarray)
+
+
+def test_decision_function_3d():
+    """Tests the decision function property for Naive Bayes for 3d config."""
+
+    clf = NaiveBayes(is_3d=True)
+    clf.train()
+
+    assert isinstance(clf.decision_function, ndarray)
